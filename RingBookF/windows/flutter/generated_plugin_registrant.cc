734eb6dc95069c6fa4a1dc6bd2b772990b33f3e3
aryanrao7 <aryanrao43@gmail.com>
2023-08-24 12:48:31 -0500

    Final

---
+++ RingBookF/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <cbl_flutter_ce/cbl_flutter_ce.h>
+#include <file_selector_windows/file_selector_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  CblFlutterCeRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CblFlutterCe"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+}
